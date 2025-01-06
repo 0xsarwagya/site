750f77bbdbe07a76cbb41f5cfd412ebf5aded45b
0xsarwagya <contact@0xsarwagya.codes>
2025-01-06 20:41:35 +0100

    chore(blog): added new blog [skip ci]
    - Added blog about "User Privacy and Location Handling in Arunya"

--- components/blog/grid.tsx
+++ components/blog/grid.tsx
@@ -2,10 +2,16 @@ import { allPosts } from "content-collections";
 import Link from "next/link";
 
 export const BlogDisplay = () => {
+	const arrangedPosts = allPosts.sort((a, b) => {
+		const dateA = new Date(a.date);
+		const dateB = new Date(b.date);
+		return dateB.getTime() - dateA.getTime();
+	});
+
 	return (
 		<section className="relative border p-4 md:p-8 lg:p-12">
 			<div className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
-				{allPosts.map((post) => (
+				{arrangedPosts.map((post) => (
 					<Link
 						className="px-6 py-4"
 						key={post.title}
